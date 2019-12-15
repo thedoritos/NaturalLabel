88dddbd140609688611d98bc235586d4dcaf2a6a
thedoritos <thedoritos@gmail.com>
2019-12-15 17:55:34 +0900

    Use NaturalLabel

---
+++ NaturalLabel/NaturalLabel.swift
@@ -0,0 +1,12 @@
+//
+//  NaturalLabel.swift
+//  NaturalLabel
+//
+//  Created by thedoritos on 2019/12/15.
+//  Copyright © 2019 KakeraGames. All rights reserved.
+//
+
+import UIKit
+
+class NaturalLabel: UILabel {
+}
