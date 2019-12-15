bc42c97cb1aa558fe3ad1ccd22e2d28137007b1b
thedoritos <thedoritos@gmail.com>
2019-12-16 02:10:42 +0900

    Calculate number of lines

--- NaturalLabel/NaturalLabel.swift
+++ NaturalLabel/NaturalLabel.swift
@@ -10,6 +10,25 @@ import UIKit
 import NaturalLanguage
 
 class NaturalLabel: UILabel {
+    private func getLines(from text: String) -> [String] {
+        guard let font = self.font else { return [] }
+
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(.font, value: font, range: NSRange(location: 0, length: attributedString.length))
+
+        let frameSetter = CTFramesetterCreateWithAttributedString(attributedString)
+
+        let framePath = CGPath(rect: CGRect(x: 0, y: 0, width: self.frame.width, height: CGFloat.greatestFiniteMagnitude), transform: nil)
+        let frame = CTFramesetterCreateFrame(frameSetter, CFRange(), framePath, nil)
+
+        guard let lines = CTFrameGetLines(frame) as? [CTLine] else { return [] }
+        return lines.map({ line in
+            let cfRange = CTLineGetStringRange(line)
+            let nsRange = NSRange(location: cfRange.location, length: cfRange.length)
+            return NSString(string: text).substring(with: nsRange)
+        })
+    }
+
     func naturalize() {
         guard let text = self.text else { return }
         debugPrint(text)
